04c946ecbb4be10f53eb0431b981bb11ec82a3fe
justinmatt <justinmatt28@gmail.com>
2024-09-23 02:39:04 +0200

    tutorial notebook added

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ This is a neural network used for finding close encounter to the Sun using Gaia 
 
 1. Open terminal or cmd.
 2. Navigate to folder containing run_prediction.py
-3. `python ./run_predict.py --inputs_X 'path to input features of the network' --scaling_factor 'path to the rescaling file used for training' --save_results 'path to csv file that stores the predicted results'`
+3. `python ./run_predict.py --inputs_X <path to input features of the network> --scaling_factor <path to the rescaling file used for training> --save_results <path to csv file that stores the predicted results>`
 
 The saved weights and corresponding scaling factor is stored in the folder `saved_model`. The default values (path towards the scaling factor and model weights) are given for `--scaling_factor` and `saved_weight`.
--- run_predict.py
+++ run_predict.py
@@ -44,19 +44,28 @@ parser = argparse.ArgumentParser(description='Process command line arguments')
 parser.add_argument('--inputs_X', type=str, default=None, help='Path to file containing input data (X)')
 parser.add_argument('--scaling_factor', type=str, default='./saved_models/rescale_factor/encounter_params_30-06_MAE_kpc_tanh_relu_split_ratio_0.1_rnd_state_10_layers_3_node_300_btch_size_512_lr_0.0001_epchs_10000.pkl', help='Path to rescaling factor')
 parser.add_argument('--saved_weight', type=str, default='./saved_models/encounter_params_full_5Mdata__30-06_MAE_kpc_tanh_relu_split_ratio_0.1_rnd_state_10_layers_3_node_300_btch_size_512_lr_0.0001_epchs_10000.h5', help='Path to saved weights')
-parser.add_argument('--ouput_file', type=str, default=None, help='Path to save predictions')
+parser.add_argument('--output_file', type=str, default=None, help='Path to save predictions')
 # Parse the command line arguments
 args = parser.parse_args()
 #load data
+print('Loading data...\n')
 xtest = pd.read_csv(args.inputs_X)
+#convert all columns to lower case
+xtest.columns = xtest.columns.str.lower()
+#store source_id in seperate variable
 
+source_id = xtest['source_id'].values
+
+#input columns
 xtest = xtest[['ra', 'dec', 'parallax', 'pmra', 'pmdec', 'radial_velocity',
        'parallax_error', 'pmra_error', 'pmdec_error', 'radial_velocity_error',
        'parallax_pmra_corr', 'parallax_pmdec_corr', 'pmra_pmdec_corr']]
 
+# Load the saved rescaling factor
 rescaler = DataPreprocessor()
 rescaler.load_scalers(args.scaling_factor)
 
+# Rescale the input data
 xtest_scl = rescaler.transform_input(xtest)
 
 # Load the saved model
@@ -66,37 +75,42 @@ activation_func = 'relu'
 num_inputs = 13
 num_outputs = 9
 
+#create model frame
 EncModel = create_neural_network(num_inputs, layers, nodes, activation_func, num_outputs)  
 
-
+#load trained weights
 EncModel.load_weights(args.saved_weight)
 
+# Predict the output
 ypred = rescaler.inverse_transform_output(EncModel.predict(xtest_scl))
 
-
+#take antilog of the output (during training we took log of the output)
 ypred[:,0] = np.exp(ypred[:,0])
 ypred[:,1] = np.exp(ypred[:,1])
 ypred[:,2] = np.exp(ypred[:,2])
 ypred[:,3] = np.exp(ypred[:,3])
 ypred[:,5] = np.exp(ypred[:,5])
 
+#store the results in a dataframe
 ypred_df = pd.DataFrame({
-    'dph_med': ypred[:, 0]*1000,
-    'dph_std': ypred[:, 1]*1000,
-    'vph_med': ypred[:, 2],
-    'vph_std': ypred[:, 3],
-    'tph_med': ypred[:, 4]/1000,
-    'tph_std': ypred[:, 5]/1000,
-    'tph_dph_corr': ypred[:, 6],
+    'source_id': source_id,
+    'dph_med': ypred[:, 0]*1000, #in pc
+    'dph_std': ypred[:, 1]*1000, #in pc
+    'vph_med': ypred[:, 2], #in km/s
+    'vph_std': ypred[:, 3], #in km/s
+    'tph_med': ypred[:, 4]/1000, #in Kyr
+    'tph_std': ypred[:, 5]/1000, #in Kyr
+    'tph_dph_corr': ypred[:, 6], 
     'tph_vph_corr': ypred[:, 7],
     'dph_vph_corr': ypred[:, 8]
 })
 
-if args.ouput_file is not None:
-    print('Saving results')
-    ypred_df.to_csv(args.output_file)
+# Save the results to a file if an output file is provided
+if args.output_file is not None:
+    print('Saving results..')
+    ypred_df.to_csv(args.output_file, index=False)
 else:
-    print(ypred_df)
+    print(ypred_df) #print the results
 
 
 
